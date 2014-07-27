86eddd30465cdd2aa1cdf80e77b5ef25d298ca15
Enrico Benini <benkio89@gmail.com>
2014-07-27 16:27:07 +0200

    Deleted Master-Worker stuff

--- src/main/it/unibo/pap/nbodies/model/force/ForceCalculator.scala
+++ src/main/it/unibo/pap/nbodies/model/force/ForceCalculator.scala
@@ -10,38 +10,10 @@ import scala.actors.ActorRef
 import akka.actor.Terminated
 
 class ForceCalculator(bodiesNumber: Int) extends Actor {
-
-  if (ForceCalculationConstants.bodiesWindowCondition(bodiesNumber)) {
-    context.become(Master)
-    context.actorOf(Props(new ForceCalculator(ForceCalculationConstants.bodiesWindow)))
-    createWorkers(bodiesNumber - ForceCalculationConstants.bodiesWindow)
-  } else {
-    context.become(Worker)
-  }
-
-  /**
-   * Create new child of the same type of this class, but in the init process they will became workers
-   */
-  private def createWorkers(b: Int) {
-    if (ForceCalculationConstants.bodiesWindowCondition(b)) {
-      context.actorOf(Props(new ForceCalculator(ForceCalculationConstants.bodiesWindow)))
-      createWorkers(b - ForceCalculationConstants.bodiesWindow)
-    } else {
-      context.actorOf(Props(new ForceCalculator(b)))
-    }
-  }
-
   // ActorRef to reply, Coordinate, mass, force to calculate
   var bodiesDetails = List[(ActorRef, Point2D, Double, Point2D)]()
 
   override def receive = {
-    case _ => println("Error: The forceCalculator has an undefined behaviour")
-  }
-
-  /**
-   * TODO: Implement
-   */
-  def Worker: Receive = {
     case CalculateForce(coordinate, mass) => {
       bodiesDetails.::((sender(), coordinate, mass, compute()))
       if (bodiesDetails.length == bodiesNumber) {
@@ -50,17 +22,6 @@ class ForceCalculator(bodiesNumber: Int) extends Actor {
       }
     }
   }
-  /**
-   * TODO: Implement
-   */
-  def Master: Receive = {
-    case CalculateForce(coordinate, mass) =>
-    case Terminated(_) => {
-      if (context.children.isEmpty)
-        context stop self
-    }
-  }
-
   private def compute() {
     var newBodyForce = new Point2D.Double(0, 0)
     bodiesDetails.foreach(body => {
